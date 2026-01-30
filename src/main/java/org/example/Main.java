e2878824fb6145d7e21ff04f7a53e8587f8e25d4
AN20662087 <anujadeshpande1996@gmail.com>
2026-01-30 15:46:04 +0530

    intial stage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import com.apps.quantityMeasurement.QuantityMeasurementApp;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -15,7 +13,6 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
 
-            QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(25);
         }
     }
 }
