080dbef26c48afd1c0c8903b9b24758d7cc3030a
AN20662087 <anujadeshpande1996@gmail.com>
2026-02-05 17:41:56 +0530

    uc7

---
+++ src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,18 @@
+package com.apps.quantityMeasurement;
+import com.app.quantitymeasurement.Length;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+        additionDemonstration(new Length(72, Length.LengthUnit.INCHES),new Length(6, Length.LengthUnit.FEET), Length.LengthUnit.YARDS);
+
+    }
+
+    public static Length additionDemonstration(Length lengthOne, Length lengthTwo, Length.LengthUnit targetUnit) {
+        Length result = Length.add(lengthOne,lengthTwo,targetUnit);
+        System.out.println("Addition of " + lengthOne.getValue() + " "+ lengthOne.getUnit() + " and " + lengthTwo.getValue() +" "+ lengthTwo.getUnit()+ " is " + result.getValue() + " "+result.getUnit());
+        //  Length lengthResult = new Length(result,lengthOne.getUnit());
+        return result;
+    }
+}
